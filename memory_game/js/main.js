172e31ceac4b6a209108daed2fc0d88dcebb4978
Jessa Marie Barre <jmlbarre@gmail.com>
2017-06-20 11:51:45 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+   {
+       rank: 'queen',
+       suit: 'hearts',
+       cardImage: 'images/queen-of-hearts.png'
+   },
+   {
+       rank: 'queen',
+       suit: 'diamonds',
+       cardImage: 'images/queen-of-diamonds.png'
+   },
+   {
+       rank: 'king',
+       suit: 'hearts',
+       cardImage: 'images/king-of-hearts.png'
+   },
+   {
+       rank: 'king',
+       suit: 'diamonds',
+       cardImage: 'images/king-of-diamonds.png'
+   }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -18,11 +40,13 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 
-	var cardOne = cards[0];
+	cardsInPlay.push(cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
+	console.log(cards[cardId].cardImage);
+
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 
