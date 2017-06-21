eaf1c015484458f227f8d328737f7c0b8926ccc5
Jessa Marie Barre <jmlbarre@gmail.com>
2017-06-21 11:19:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,9 @@ var cards = [
 
 var cardsInPlay = [];
 
+var wins = 0;
+var losses = 0;
+
 var checkForMatch = function() {
 
 	if (cardsInPlay.length === 2) {
@@ -31,27 +34,80 @@ var checkForMatch = function() {
 
 			 alert("You found a match!");
 
+			 wins += 1;
+
+			 document.getElementById('wins').textContent = wins;
+
 		} else {
 
 			 alert("Sorry, try again.");
+
+			 losses += 1;
+
+			 document.getElementById('losses').textContent = losses;
 		}
 	}
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
+
+	//console.log("User flipped " + cards[cardId].rank);
+
+	//console.log(cards[cardId].cardImage);
+
+	//console.log(cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId].rank);
-
-	console.log(cards[cardId].cardImage);
-
-	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	checkForMatch();
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+
+	for (var i = 0; i < cards.length; i++) {
+
+		/* create card elements */
+
+		var cardElement = document.createElement('img');
+		
+		cardElement.setAttribute('src', 'images/back.png');
+		
+		cardElement.setAttribute('data-id', i);
+		
+		document.getElementById('game-board').appendChild(cardElement);
+		
+		cardElement.addEventListener('click', flipCard);
+		
+		/* add reset button listener */
+
+		document.getElementById('reset').addEventListener('click', reset);
+
+	}
+};
+
+var reset = function() {
+
+	var node = document.getElementById('game-board');
+
+	/* clear game board and cards in play */
+
+	while (node.hasChildNodes()) {
+   	 node.removeChild(node.lastChild);
+
+    }
+
+    while(cardsInPlay.length != 0) {
+    	cardsInPlay.pop();
+    }
+
+	createBoard();
+}
+
+createBoard();
+
 
